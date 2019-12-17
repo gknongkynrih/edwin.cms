d27b57b6105df42ea99bf84f8b521cac04f46fb1
GOrdon <gordon.nongkynrih@outlook.com>
2019-12-17 09:58:33 +0530

    admin layout

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//clear
+//Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('admin/user', 'AdminUsersController');
+
